c4d8ce30f2ce013202d4ef7f2139f84fe075de92
Raymond Lochner <ray_lochner@yahoo.de>
2016-11-01 11:53:11 +0100

    .gitignore and java class

---
+++ src/com/github/aldar_najim_raymond/TempMainClass.java
@@ -0,0 +1,9 @@
+package com.github.aldar_najim_raymond;
+
+public class TempMainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+}
