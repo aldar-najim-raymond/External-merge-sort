93379318efb8bf0c126df3dca2dfad444f301d40
Raymond Lochner <ray_lochner@yahoo.de>
2016-11-13 12:03:06 +0100

    Implementing readInt() function for the buffered solution

--- src/com/github/aldar_najim_raymond/ReaderWriterMemoryBuffer.java
+++ src/com/github/aldar_najim_raymond/ReaderWriterMemoryBuffer.java
@@ -6,98 +6,98 @@ import java.io.DataInputStream;
 import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class ReaderWriterMemoryBuffer {
+public class ReaderWriterMemoryBuffer extends AbstractReaderWriter {
 
 	public static int defaultMemoryBufferSize = 4;
 
+	private InputStream is;
+	private DataInputStream ds;
+
 	private OutputStream os;
-	private int memoryBufferSize;
 
 	/*
-	 * constructors with/without buffer size specified
+	 * byte[] buffer
 	 */
-	public ReaderWriterMemoryBuffer() {
-		this.memoryBufferSize = ReaderWriterMemoryBuffer.defaultMemoryBufferSize;
+	private byte[] memoryBuffer;
+	/*
+	 * Points to the current value in the buffer
+	 */
+	private int bufferPointer;
+	private int bufferLength;
+
+	public ReaderWriterMemoryBuffer(String file, IOType type) {
+		this(file, type, ReaderWriterMemoryBuffer.defaultMemoryBufferSize);
 	}
 
-	public ReaderWriterMemoryBuffer(int bufferSize) {
-		this.builderSetMemoryBufferSize(bufferSize);
+	public ReaderWriterMemoryBuffer(String file, IOType type, int bufferSize) {
+		super(file, type);
+		this.setMemoryBufferSize(bufferSize);
+
+		/*
+		 * Initializing read streams
+		 */
+		if (this.getType() == IOType.READ) {
+			try {
+				is = new FileInputStream(new File(file));
+				ds = new DataInputStream(is);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			this.bufferLength = -2;
+			/*
+			 * Initializing write streams
+			 */
+		} else if (this.getType() == IOType.WRITE) {
+			try {
+				os = new FileOutputStream(file);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		} else {
+			throw new UnsupportedOperationException(this.getType() + " not supported");
+		}
+		this.bufferPointer = 0;
 	}
 
 	/*
-	 * Set buffer size as builder pattern method
+	 * Set buffer size
 	 */
-	public ReaderWriterMemoryBuffer builderSetMemoryBufferSize(int size) {
+	private void setMemoryBufferSize(int size) {
 		if (size < 1) {
-			throw new IllegalArgumentException(
-					"Memory buffer size must be larger than 0.");
+			throw new IllegalArgumentException("Memory buffer size must be larger than 0.");
 		}
 		/*
 		 * only allow multiples of 4 to make the write/read methods easier ...
 		 */
 		else if ((size % 4) != 0) {
-			throw new IllegalArgumentException(
-					"Buffer size must be multiple of 4");
+			throw new IllegalArgumentException("Buffer size must be multiple of 4");
 		}
-		this.memoryBufferSize = size;
-		return this;
+		this.memoryBuffer = new byte[size];
 	}
 
-	/*
-	 * Read the input stream with a memory buffer
-	 */
-	public void read(String file) {
-		InputStream is;
-		byte[] readBuffer;
-		try {
-			is = new FileInputStream(new File(file));
-			DataInputStream ds = new DataInputStream(is);
-			readBuffer = new byte[this.memoryBufferSize];
-			int length = 0;
-			try {
-				/*
-				 * length = number of bytes read
-				 */
-				while ((length = ds.read(readBuffer)) != -1) {
-					/*
-					 * Transforming the byte[] into integers and printing them
-					 * out
-					 */
-					for (int i = 0; i < length; i += 4) {
-						byte[] number = new byte[4];
-						System.arraycopy(readBuffer, i, number, 0, 4);
-						System.out.println(UtilisationClass
-								.ByteArrayToInt(number));
-					}
-				}
-			} catch (EOFException e) {
-				ds.close();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
 
 	/*
 	 * simple write with n-integer random integers
 	 */
+	
 	public void write(String file, int integers) {
 		byte[] buffer;
 		int bufferCounter = 0;
 		try {
 			os = new FileOutputStream(file);
-			buffer = new byte[this.memoryBufferSize];
+			//buffer = new byte[this.memoryBufferSize];
 			for (int i = 0; i < integers; i++) {
 
-				byte[] number = UtilisationClass
-						.IntToByteArray(UtilisationClass.randomNumber());
+				byte[] number = UtilisationClass.IntToByteArray(UtilisationClass.randomNumber());
 
-				System.arraycopy(number, 0, buffer, bufferCounter, 4);
+				//System.arraycopy(number, 0, buffer, bufferCounter, 4);
 				/*
 				 * increase counter by 4 as one number is 4 bytes
 				 */
@@ -105,19 +105,21 @@ public class ReaderWriterMemoryBuffer {
 				/*
 				 * Buffer is full -> write data to file
 				 */
+				/*
 				if (bufferCounter >= this.memoryBufferSize) {
 					os.write(buffer);
 					bufferCounter = 0;
-				}
+				}*/
 			}
 
 			/*
 			 * check if there is still data in the buffer not yet written to the
 			 * file
 			 */
+			/*
 			if (bufferCounter != 0) {
 				os.write(buffer, 0, bufferCounter);
-			}
+			}*/
 
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -125,14 +127,49 @@ public class ReaderWriterMemoryBuffer {
 	}
 
 	/*
-	 * Buffer size get/set
+	 * Read the input stream with a memory buffer
 	 */
-	public void setMemoryBufferSize(int size) {
-		this.builderSetMemoryBufferSize(size);
+	@Override
+	public int readInt() throws IOException {
+		/*
+		 * First read
+		 */
+		if (this.bufferLength == -2) {
+			this.bufferLength = ds.read(this.memoryBuffer);
+		} 
+		/*
+		 * Read n memory bytes from stream when the pointer is larger than the buffer
+		 */
+		else if (this.bufferPointer >= this.memoryBuffer.length){
+			this.bufferLength = ds.read(this.memoryBuffer);
+			this.bufferPointer = 0;
+		}
+		if (this.bufferLength == -1) {
+			throw new EOFException();
+		}
+		/*
+		 * Copy integer from the memory buffer at the pointer position
+		 */
+		byte[] number = new byte[4];
+		System.arraycopy(this.memoryBuffer, this.bufferPointer, number, 0, 4);
+		this.bufferPointer += 4;
+		return UtilisationClass.ByteArrayToInt(number);
 	}
 
-	public int getMemoryBufferSize() {
-		return this.memoryBufferSize;
+	@Override
+	public void writeInt(int number) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void closeStream() throws IOException {
+		if (this.getType() == IOType.READ) {
+			ds.close();
+		} else if (this.getType() == IOType.WRITE) {
+			//bos.close();
+		}
+
 	}
 
 }
